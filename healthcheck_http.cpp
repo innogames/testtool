afe0ae6cd6f9e410f02e8a88b05f4fdf72a8a0c8
Emre Hasegeli <emre@hasegeli.com>
2016-05-30 14:42:27 +0200

    Refactor event_callback() of healthcheck_http.cpp

--- healthcheck_http.cpp
+++ healthcheck_http.cpp
@@ -33,34 +33,43 @@ void Healthcheck_http::confline_callback(string &var, istringstream &val) {
 }
 
 /*
-   Constructor for HTTP healthcheck. Parses http(s)-specific parameters.
-*/
+ * Constructor for HTTP healthcheck
+ *
+ * Parses http(s)-specific parameters.
+ */
 Healthcheck_http::Healthcheck_http(istringstream &definition, class LbNode *_parent_lbnode): Healthcheck(definition, _parent_lbnode) {
-	/* Initialize pointers to NULL, this is not done automatically. */
+
+	// This is not done automatically.
 	bev = NULL;
-	/* Set defaults. */
-	this->port = 80;
+
+	// Set defaults
+	if (this->port == 0)
+		this->port = 80;
 
 	this->read_confline(definition);
 
-	/* If host was not given, use IP address. */
+	// If host was not given, use IP address
 	if (host == "")
 		host = parent_lbnode->address.c_str();
 
-	/* Copy address to addrinfo struct for connect. I am aware that converting port number to text and back to int is stupid. */
+	/*
+	 * Copy address to addrinfo struct for connect
+	 *
+	 * I am aware that converting port number to text and back to int is stupid.
+	 */
 	memset(&addrinfo, 0, sizeof(addrinfo));
 	char port_str[256];
 	memset(port_str, 0, sizeof(port_str));
 	snprintf(port_str, sizeof(port_str), "%d", port);
 	getaddrinfo(parent_lbnode->address.c_str(), port_str, NULL, &addrinfo);
-	
-	/* Split the list by ".", copy each found code into the vector. */
+
+	// Split the list by ".", copy each found code into the vector
 	stringstream ss_http_ok_codes(this->st_http_ok_codes);
 	string http_ok_code;
 	while(getline(ss_http_ok_codes, http_ok_code, '.'))
 		http_ok_codes.push_back(http_ok_code);
 
-	/* Print codes as they were parsed. */
+	// Print codes as they were parsed
 	char codes_buf[1024];
 	int offset = 0;
 	for (unsigned int i = 0; i<http_ok_codes.size(); i++) {
@@ -72,14 +81,17 @@ Healthcheck_http::Healthcheck_http(istringstream &definition, class LbNode *_par
 }
 
 /*
-   Constructor for HTTPS healthcheck. It only calls HTTP constructor. Now that's what I call inheritance!
-*/
+ * Constructor for HTTPS healthcheck
+ *
+ * It only calls HTTP constructor.  Now that's what I call inheritance!
+ */
 Healthcheck_https::Healthcheck_https(istringstream &definition, class LbNode *_parent_lbnode): Healthcheck_http(definition, _parent_lbnode) {
 	type = "https";
 }
 
 int Healthcheck_http::schedule_healthcheck(struct timespec *now) {
-	/* Peform general stuff for scheduled healthcheck. */
+
+	// Peform general stuff for scheduled healthcheck
 	if (Healthcheck::schedule_healthcheck(now) == false)
 		return false;
 
@@ -104,7 +116,7 @@ int Healthcheck_http::schedule_healthcheck(struct timespec *now) {
 int Healthcheck_https::schedule_healthcheck(struct timespec *now) {
 	SSL *ssl;
 
-	/* Peform general stuff for scheduled healthcheck. */
+	// Peform general stuff for scheduled healthcheck
 	if (Healthcheck::schedule_healthcheck(now) == false)
 		return false;
 
@@ -140,97 +152,68 @@ void Healthcheck_http::read_callback(struct bufferevent *bev, void * arg) {
 }
 
 /*
-   The callback is called by libevent, it's a static method that requires the Healthcheck object to be passed to it.
+ * The callback is called by libevent
+ *
+ * It's a static method that requires the Healthcheck object to be passed to it.
 */
 void Healthcheck_http::event_callback(struct bufferevent *bev, short events, void *arg) {
 	(void)(bev);
 	Healthcheck_http *hc = (Healthcheck_http *)arg;
 
-	/* Ignore READING, WRITING, CONNECTED events. */
-	if (events & (BEV_EVENT_ERROR|BEV_EVENT_TIMEOUT|BEV_EVENT_EOF)) {
+	// Ignore READING, WRITING, CONNECTED events
+	if (!(events & (BEV_EVENT_ERROR|BEV_EVENT_TIMEOUT|BEV_EVENT_EOF)))
+		return;
 
-		if (events & BEV_EVENT_TIMEOUT) {
-			hc->last_state = STATE_DOWN;
-			if (verbose>1 || hc->hard_state != STATE_DOWN)
-				log_lb(MSG_TYPE_HC_FAIL,
-				    hc->parent_lbnode->parent_lbpool->name.c_str(),
-				    hc->parent_lbnode->address.c_str(),
-				    hc->port,
-				    "Healthcheck_%s: timeout after %d,%03ds; message: %s",
-				    hc->type.c_str(),
-				    hc->timeout.tv_sec,
-				    hc->timeout.tv_usec / 1000,
-				    evutil_socket_error_to_string(EVUTIL_SOCKET_ERROR()));
+	if (events & BEV_EVENT_TIMEOUT)
+		return hc->end_check(HC_FAIL, "timeout");
 
-		}
-		else if (events & BEV_EVENT_ERROR) {
-			hc->last_state = STATE_DOWN;
-			if (verbose>1 || hc->hard_state != STATE_DOWN)
-				log_lb(MSG_TYPE_HC_FAIL,
-				    hc->parent_lbnode->parent_lbpool->name.c_str(),
-				    hc->parent_lbnode->address.c_str(),
-				    hc->port,
-				    "Healthcheck_%s: error connecting; message: %s",
-				    hc->type.c_str(),
-				    evutil_socket_error_to_string(EVUTIL_SOCKET_ERROR()));
-		}
-		else if (events & BEV_EVENT_EOF) {
-			/* Get 1st line of reply. */
-			stringstream replystream(hc->reply);
-			string statusline;
-			getline(replystream, statusline);
+	if (events & BEV_EVENT_ERROR)
+		return hc->end_check(HC_FAIL, "connection error");
 
-			/* A line goes like this:
-			 * HTTP/1.1 200 OK
-			 * Http code is the string between 1st and 2nd ' '.
-			 * It would be awesome to use regexp but then
-			 * we need boost or C++11 which is not in FreeBSD 9 (but is in 10).
-			 */ 
-			int pos;
-			pos = statusline.find(" ");
-			statusline = statusline.substr(pos+1);
-			pos = statusline.find(" ");
-			statusline = statusline.substr(0, pos);
+	// Get 1st line of reply
+	stringstream replystream(hc->reply);
+	string statusline;
+	getline(replystream, statusline);
 
-			unsigned int i;
+	/*
+	 * This first line goes like this:
+	 *
+	 *	HTTP/1.1 200 OK
+	 *
+	 * HTTP code is the string between 1st and 2nd " ".
+	 */
+	int pos;
+	pos = statusline.find(" ");
+	statusline = statusline.substr(pos + 1);
+	pos = statusline.find(" ");
+	statusline = statusline.substr(0, pos);
 
-			for (i = 0; i<hc->http_ok_codes.size(); i++) {
-				if (statusline.compare(hc->http_ok_codes[i]) == 0) {
-					if (verbose>1 || hc->last_state == STATE_DOWN)
-						log_lb(MSG_TYPE_HC_PASS,
-						    hc->parent_lbnode->parent_lbpool->name.c_str(),
-						    hc->parent_lbnode->address.c_str(),
-						    hc->port,
-						    "Healthcheck_%s: good HTTP code: %s",
-						    hc->type.c_str(),
-						    statusline.c_str());
-	
-					hc->last_state = STATE_UP;
-					break;
-				}
-			}
-		
-			if (i == hc->http_ok_codes.size()) {
-				hc->last_state = STATE_DOWN;
-				if (verbose>1 || hc->hard_state != STATE_DOWN) {
-					log_lb(MSG_TYPE_HC_FAIL,
-					    hc->parent_lbnode->parent_lbpool->name.c_str(),
-					    hc->parent_lbnode->address.c_str(),
-					    hc->port,
-					    "Healthcheck_%s: bad HTTP code: %s",
-					    hc->type.c_str(),
-					    statusline.c_str());
-				}
-			}
-		}
+	std::stringstream message;
+	message << "HTTP code " << statusline;
 
-		hc->cleanup_connection();
-		hc->handle_result();
-	}
+	unsigned int i;
+	for (i = 0; i<hc->http_ok_codes.size(); i++)
+		if (statusline.compare(hc->http_ok_codes[i]) == 0)
+			return hc->end_check(HC_PASS, message.str());
+
+	return hc->end_check(HC_FAIL, message.str());
 }
 
-void Healthcheck_http::cleanup_connection() {
-	if (bev)
-		bufferevent_free(bev);
-	bev = NULL;
+/*
+ * Override end_check() method to clean up things
+ */
+void Healthcheck_http::end_check(HealthcheckResult result, string message) {
+	if (verbose >= 2 && result != HC_PASS && this->bev != NULL) {
+		char *error = evutil_socket_error_to_string(EVUTIL_SOCKET_ERROR());
+
+		if (error != NULL && strlen(error) > 0)
+			log_txt(MSG_TYPE_DEBUG, "socket error: %s", error);
+	}
+
+	if (this->bev != NULL) {
+		bufferevent_free(this->bev);
+		this->bev = NULL;
+	}
+
+	Healthcheck::end_check(result, message);
 }
